bc007d362ce72e4c482ae999789d5f9b5aeda037
borislavstoychev <stoy4ew@gmail.com>
2021-12-18 13:55:05 +0200

    added id

--- tool/serializers.py
+++ tool/serializers.py
@@ -13,6 +13,7 @@ class CandidateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidate
         fields = (
+            'id',
             'first_name', 
             'last_name', 
             'email', 
